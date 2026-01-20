f75e586fb21083ee52519a3dafba79bcaf604bcf
sahan200401 <jbasnethmina@gmail.com>
2026-01-20 17:11:59 +0530

    update the mse and r2_squard..

--- SRC/train_model.py
+++ SRC/train_model.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import joblib
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from preprocessing import preprocess_data
-from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.metrics import mean_squared_error, r2_score
+
 
 
 df = pd.read_csv(r"C:\Users\jbasn\OneDrive\Documents\winde_data_set\Data\Raw\winequalityN.csv")
@@ -17,7 +19,8 @@ model.fit(x_train,y_train)
 
 y_pred = model.predict(x_test)
 
-rmse = mean_squared_error(y_test,y_pred,squared = False)
+mse = mean_squared_error(y_test,y_pred)
+rmse = np.sqrt(mse)
 r2 = r2_score(y_test,y_pred)
 
 print("RMSE : ", rmse)
